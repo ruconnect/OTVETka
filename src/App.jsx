b7de412b37d0583c942bcaaab494d4824a8159b1
Mickredset <aivliev782@gmail.com>
2026-09-06 21:36:28 +0300

    Update

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="app">
+      <meta http-equiv="refresh" content="0; url=home.html"></meta>
+      <h1>html</h1>
+      <p>/home.html</p>
+    </div>
+  );
+}
